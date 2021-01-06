b0bfe27b9b3055544fe9ca9fd3dfe044dd95b3f6
bhavesh <bkpassi2017@gmail.com>
2021-01-06 19:19:38 +0530

    Initial Commit 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
+import Main from './components/MainComponent'
 import './App.css';
 
 class App extends Component {
@@ -20,55 +7,12 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.toggleNav = this.toggleNav.bind(this);
-    this.state = {
-      isNavOpen: false
-    };
-  }
-
-  toggleNav() {
-    this.setState({
-      isNavOpen: !this.state.isNavOpen
-    })
   }
 
   render() {
     return (
       <div className="App">
-        <div >
-            <Navbar color="light" light expand="md">
-            <NavbarBrand href="/">CollegeQuora</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNav} />
-            <Collapse isOpen={this.state.isNavOpen} navbar>
-              <Nav className="mr-auto" navbar>
-                <NavItem>
-                  <NavLink href="/components/">Components</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    Options
-                  </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem>
-                      Option 1
-                    </DropdownItem>
-                    <DropdownItem>
-                      Option 2
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                      Reset
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-              </Nav>
-              <NavbarText>Simple Text</NavbarText>
-            </Collapse>
-          </Navbar>
-        </div>
+        <Main />
       </div>
     );
   }
---
+++ src/components/MainComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './HeaderComponent';
+
+class Main extends Component {
+
+    constructor(props) {
+
+        super(props);
+    }
+
+    render() {
+        return(
+            <Header />
+        );   
+    }
+}
+
+export default Main
