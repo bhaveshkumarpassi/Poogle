087bf941335191f771b2a7d4d8f0caf75cd985ec
Utkarsh Goel <ugoel911@gmail.com>
2021-01-15 13:49:47 +0530

    bootstrap added

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,47 +1,62 @@
 import React from "react";
 import "./Chat.css";
-import { Image, ListGroup, ListGroupItem } from "react-bootstrap";
+import {
+	Container,
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	ListGroupItem,
+} from "react-bootstrap";
 import profilePic from "../../Images/profile_pic.png";
 
 const Chat = (props) => {
 	const chats = [
-		{ name: "Dips", chat: [{ msg: "Hello", sender: "me" }] },
+		{ name: "Dips", chat: [{ msg: "B", sender: "me" }] },
 		{ name: "Bhav", chat: [{ msg: "Bye", sender: "other" }] },
 		{ name: "Nisha", chat: [{ msg: "Yo", sender: "me" }] },
 	];
 
+	const personChat = ({ name, chat }) => (
+		<ListGroupItem
+			style={{ backgroundColor: "#a56cc1", border: "0px" }}
+			className="person"
+		>
+			<Row>
+				<Col>
+					<Image
+						src={profilePic}
+						className="user__profile__pic"
+						roundedCircle
+						style={{
+							maxHeight: "70px",
+						}}
+					/>
+				</Col>
+				<Col>
+					<h3>{name}</h3>
+					<p className="display__chat">{chat[0].msg}</p>
+				</Col>
+			</Row>
+		</ListGroupItem>
+	);
+
 	return (
-		<div className="grid">
-			<div className="col-1">Search person</div>
-			<div className="col-2">
-				<ListGroup style={{ backgroundColor: "#a56cc1" }}>
-					{chats.map(({ name, chat }) => (
-						<ListGroupItem
-							style={{ backgroundColor: "#a56cc1", border: "0px" }}
-							className="person"
-						>
-							<div className="image">
-								<Image
-									src={profilePic}
-									className="user__profile__pic"
-									roundedCircle
-									style={{
-										maxHeight: "70px",
-									}}
-								/>
-							</div>
-							<div className="name">
-								<h3>{name}</h3>
-							</div>
-							<div className="display__chat">
-								<p className="display__chat">{chat[0].msg}</p>
-							</div>
-						</ListGroupItem>
-					))}
-				</ListGroup>
-			</div>
-			<div className="col-3">Chat</div>
-		</div>
+		<Container>
+			<Row>
+				<Col>
+					<Row>Search person</Row>
+					<Row>
+						<ListGroup style={{ backgroundColor: "#a56cc1" }}>
+							{chats.map(personChat)}
+						</ListGroup>
+					</Row>
+				</Col>
+				<Col>
+					<div className="col-3">Chat</div>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
