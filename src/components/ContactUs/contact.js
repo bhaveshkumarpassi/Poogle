28fb86ad2472b2e7e7aaa78c0f324f7ca5487171
Deepanshu Jindal <43994883+ultimatecoder2@users.noreply.github.com>
2021-01-22 10:27:40 +0530

    Contactus form vallidation
    Added form validation for contactus form

--- src/components/ContactUs/contact.js
+++ src/components/ContactUs/contact.js
@@ -12,7 +12,12 @@ class Contact extends Component {
         super(props);
         this.state = {
             email:"",
-            feedback:""
+            feedback:"", 
+            errors:{
+                email:"",
+                feedback:""
+            },
+            validated:false
         }
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,10 +29,46 @@ class Contact extends Component {
           [name]: event.target.value
         });
     }
-
+    formValidation = () =>{
+        const{email, feedback} = this.state;
+        let emailError="", feedbackError = "", error;
+        if(!email){
+            emailError = "Email is required";
+            error = true;            
+        }
+        else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email))
+        {
+            emailError = "Email address is Invalid";
+            error= true;
+        }
+        if(!feedback.trim())
+        {
+            feedbackError="Feedback is required"
+            error= true;
+        }
+        
+        this.setState(prevState => ({
+            errors:{
+                email:emailError,
+                feedback: feedbackError
+            }
+        }))
+        console.log("Errors are", this.state.errors);
+        console.log(emailError);
+        console.log(feedbackError)
+        return !error;
+    }
     handleSubmit(event) {
         event.preventDefault();
+        const isValid = this.formValidation();
+        this.setState({
+            validated:isValid
+        })
+        if(isValid){
+            window.alert("Form Submitted")
+        }
         console.log(this.state);
+        
     }
     
     
@@ -45,9 +86,7 @@ class Contact extends Component {
 						</Breadcrumb>
 						
                     </Row>
-                    {/* <Row className="page__heading">
-                        <h2>Contact Us</h2>
-                    </Row> */}
+                    
                     <div>
                         <p className="contact__text">
                         We strongly believe in improving our products and services. Any queries, feedback or suggestion are more than welcome.
@@ -57,15 +96,17 @@ class Contact extends Component {
                     <br/>
                     <div>
                         <Jumbotron className="form__content__div">
-                            <Form>
+                            <Form validated={this.state.validated}>
                                 <Form.Group controlId="formBasicEmail">
                                     <Form.Label><span className="form__icon"><AiOutlineMail/></span>Email address</Form.Label>
                                     <input name="email" className="form-control" type="email" value={this.state.name} placeholder="Enter email" onChange={this.handleInputChange} />
+                                    <div className="invalid__feedback">{this.state.errors.email}</div>
                                 </Form.Group>
 
                                 <Form.Group controlId="formBasicTextbox">
                                     <Form.Label><span className="form__icon"><BsPencilSquare/></span> Queries/ Suggestions</Form.Label>
-                                    <textarea name="feedback" rows={5} className="form-control"  value={this.state.name} placeholder="Enter your queries/ suggeestions here" onChange={this.handleInputChange} />
+                                    <textarea name="feedback" rows={5} className="form-control"  value={this.state.name} placeholder="Enter your queries/ suggestions here" onChange={this.handleInputChange} />
+                                    <div className="invalid__feedback">{this.state.errors.feedback}</div>
                                 </Form.Group>
                                 <div className="form__btn">
                                     <button className="btn contact__form__button" type="submit" onClick={this.handleSubmit}>
