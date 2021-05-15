156b20c59b38e2cc78fc3e90322dfa6fe1fe3d59
Deepanshu Jindal <deepu.jindal2002@gmail.com>
2021-05-15 14:40:35 +0530

    update baseurls

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,3 +1,4 @@
 // export const baseUrl = "http://localhost:3001/";
+// export const frontendBaseUrl = "http://localhost:3000/"
 export const baseUrl ="https://poogle-app-backend.herokuapp.com/"
 export const frontendBaseUrl = "https://poogle-app.herokuapp.com/"
